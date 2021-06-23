d5c39ddfddc75fa30b4db16abf9e6cf66707a383
AKanvinde <86362448+AKanvinde@users.noreply.github.com>
2021-06-23 13:07:36 -0400

    Add files via upload

---
+++ OOP_Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bal;
+            SalaryAccount obj = new SalaryAccount();
+            bal = obj.DepositeSalary(2000);
+            obj.SSN = 1234;
+            Console.WriteLine("Account Balance is " +bal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP_Encapsulation/SalaryAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Encapsulation
+{
+    public class SalaryAccount
+    {
+        private double acc_bal;
+
+        private int _ssn;
+        public int SSN
+        {
+            get
+            {
+                return _ssn;
+            }
+            set
+            {
+                _ssn = value;
+            }
+        }
+
+        public double DepositeSalary(int Salary)
+        {
+            acc_bal = 100000;
+            return (acc_bal + Salary);
+        }
+    }
+}
